8e306d0cc58db4dd351df5ded0cb92eb043c5117
Kailey Kaes <56466345+kaileykaes@users.noreply.github.com>
2023-03-29 09:58:44 -0600

    Test: #details

--- spec/team_spec.rb
+++ spec/team_spec.rb
@@ -73,4 +73,17 @@ RSpec.describe Team do
       expect(@team.total_value).to eq(85200000)
     end
   end
+
+  describe '#details' do 
+    it 'creates a hash of team details' do 
+      @team.add_player(@player_1)
+      @team.add_player(@player_2)
+      @team.add_player(@player_3)
+      @team.add_player(@player_4)
+      expect(@team.details).to eq({
+        "total_value" => 85200000, 
+        "player_count" => 4
+      })
+    end
+  end
 end
