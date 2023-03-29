51dd711a3582410a0dbb47ae0e60090e3a364d0f
Kailey Kaes <56466345+kaileykaes@users.noreply.github.com>
2023-03-29 09:37:10 -0600

    Test: #initialize, #add_player

---
+++ spec/team_spec.rb
@@ -0,0 +1,44 @@
+require 'rspec'
+require './lib/player'
+require './lib/team'
+
+RSpec.describe Team do
+  before(:each) do
+    @team = Team.new("Dodgers", "Los Angeles")
+    @player_1 = Player.new("Michael Palledorous" , 1000000, 36)
+    @player_2 = Player.new("Kenny DeNunez", 500000, 24)
+    @player_3 = Player.new("Alan McClennan", 750000, 48)
+    @player_4 = Player.new("Hamilton Porter", 100000, 12)
+  end
+
+  describe '#initialize' do 
+    it 'exists' do 
+      expect(@team).to be_a Team
+    end
+
+    it 'has attributes' do 
+    end
+    
+    it 'begins with an empty roster' do 
+      expect(@team.roster).to eq([])
+    end
+    
+    it 'begins with 0 players' do 
+      expect(@team.player_count).to eq(0)
+    end
+  end
+
+  describe '#add_player' do 
+    it 'adds players to roster' do 
+      @team.add_player(@player_1)
+      @team.add_player(@player_2)
+      expect(@team.roster).to eq([@player_1, @player_2])
+    end
+
+    it 'increases player count with added players' do 
+      @team.add_player(@player_1)
+      @team.add_player(@player_2)
+      expect(@team.player_count).to eq(2)
+    end
+  end
+end
