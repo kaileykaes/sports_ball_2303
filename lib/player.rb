e9d21f85dec06981e619a5ea0f6bd0f753b4d44c
Kailey Kaes <56466345+kaileykaes@users.noreply.github.com>
2023-03-29 09:21:06 -0600

    Feat: #initialize

---
+++ lib/player.rb
@@ -0,0 +1,11 @@
+class Player
+  attr_reader :name, 
+              :monthly_cost, 
+              :contract_length
+  
+  def initialize(name, monthly_cost, contract_length)
+    @name = name
+    @monthly_cost
+    @contract_length
+  end
+end
