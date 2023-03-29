0dc273219e432e0fda9a18502636a5efd7e72e6e
Kailey Kaes <56466345+kaileykaes@users.noreply.github.com>
2023-03-29 09:23:50 -0600

    Fix: #initialize

--- lib/player.rb
+++ lib/player.rb
@@ -5,11 +5,15 @@ class Player
   
   def initialize(name, monthly_cost, contract_length)
     @name = name
-    @monthly_cost
-    @contract_length
+    @monthly_cost = monthly_cost
+    @contract_length = contract_length
   end
 
   def first_name
     name.split(' ').first
+  end
+
+  def last_name
+    name.split(' ').last
   end
 end
